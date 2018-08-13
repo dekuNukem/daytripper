d73f31f4d10cefbde273cb9747f1da8588b3caa0
dekunukem <dekunukem@gmail.com>
2018-08-13 13:15:14 +0100

    2nd commit

---
+++ push.sh
@@ -0,0 +1,15 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'error: please enter a commit comment'
+    exit 1
+fi
+
+find . -type f -name "*.b#*" -exec rm -f {} \;
+find . -type f -name "*.s#*" -exec rm -f {} \;
+find . -type f -name "*.l#*" -exec rm -f {} \;
+find . -type f -name "*.csv#*" -exec rm -f {} \;
+find . -type f -name "*.DS_Store*" -exec rm -f {} \;
+find . -name "__pycache__" -exec rm -rf {} \;
+find . -type f -name "._*" -exec rm -f {} \;
+git add --all
+git commit -m "$@"
+git push origin master
