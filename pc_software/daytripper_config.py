3483c8bf371804ed2ab278df98e753e3dd4ce5c4
dekuNukem <dekunukem@gmail.com>
2020-07-15 21:13:40 +0100

    added hiding UI elecments

--- pc_software/daytripper_config.py
+++ pc_software/daytripper_config.py
@@ -36,18 +36,13 @@ daytripper_config = dt_objs.dt_config()
 # ------------- serial select -------------
 
 connection_lf = LabelFrame(root, text="Connection", width=MAIN_WINDOW_WIDTH - PADDING*2, height=HEIGHT_CONNECT_LF)
-connection_lf.pack()
 connection_lf.place(x=PADDING, y=0)
-connection_lf.pack_propagate(False) 
 root.update()
 
 serial_var = StringVar()
 serial_var.set(' ')
 OptionList = ['']
 serial_optlist = OptionMenu(connection_lf, serial_var, *OptionList)
-# serial_optlist.config(width=100)
-serial_optlist.pack()
-serial_optlist.pack_propagate(False)
 serial_optlist.place(x=115, y=0, width=240)
 ser = serial.Serial()
 ser.baudrate = 115200
@@ -108,99 +103,77 @@ def serial_connect():
         return
     print(daytripper_config)
     hwif_label_stringvar.set(make_info_string())
+    enable_widgets()
+    show_settings()
 
 def serial_apply_changes():
     pass
 
 def serial_reset_all():
-    pass
+    print("serial_reset_all")
+    refresh_rate_lf.place_forget()
+    root.update()
 
 user_guide_button = Button(connection_lf, text="User Guide...", command=open_user_manual_url)
-user_guide_button.pack()
 user_guide_button.place(x=PADDING, y=0, width=100)
-
 serial_refresh_button = Button(connection_lf, text="Refresh", command=serial_dropdown_refresh)
-serial_refresh_button.pack()
 serial_refresh_button.place(x=40+320, y=0, width=70)
-
 serial_connect_button = Button(connection_lf, text="Connect", command=serial_connect)
-serial_connect_button.pack()
 serial_connect_button.place(x=40+390, y=0, width=70)
-
-serial_apply_button = Button(connection_lf, text="Apply Changes", command=serial_apply_changes)
-serial_apply_button.pack()
+serial_apply_button = Button(connection_lf, text="Apply Changes", command=serial_apply_changes, state=DISABLED)
 serial_apply_button.place(x=40+460, y=0, width=110)
-
-serial_reset_all_button = Button(connection_lf, text="Reset All", command=serial_reset_all)
-serial_reset_all_button.pack()
+serial_reset_all_button = Button(connection_lf, text="Reset All", command=serial_reset_all, state=DISABLED)
 serial_reset_all_button.place(x=40+570, y=0, width=80)
 
-serial_dropdown_refresh()
-serial_connect()
-
 hwif_lf = LabelFrame(root, text="Hardware Info", width=MAIN_WINDOW_WIDTH - PADDING*2, height=HEIGHT_CONNECT_LF)
-hwif_lf.pack()
-hwif_lf.place(x=PADDING, y=50)
-hwif_lf.pack_propagate(False) 
-root.update()
-
 hwif_label = Label(master=hwif_lf, textvariable=hwif_label_stringvar)
-hwif_label.pack()
-hwif_label.place(x=0, y=0)
 
 SETTING_LF_HEIGHT = 300
-
 settings_lf = LabelFrame(root, text="Settings", width=MAIN_WINDOW_WIDTH - PADDING*2, height=SETTING_LF_HEIGHT)
-settings_lf.pack()
-settings_lf.place(x=PADDING, y=100)
-settings_lf.pack_propagate(False) 
-root.update()
-
 refresh_rate_lf = LabelFrame(settings_lf, text="Refresh Rate", width=int((MAIN_WINDOW_WIDTH - PADDING*2) / 4.4), height=SETTING_LF_HEIGHT/2)
-refresh_rate_lf.pack()
-refresh_rate_lf.place(x=PADDING, y=0)
-refresh_rate_lf.pack_propagate(False) 
-root.update()
-
 refresh_rate_slider_text = Label(refresh_rate_lf, text=str(daytripper_config.refresh_rate_Hz)+'Hz')
-refresh_rate_slider_text.pack()
-refresh_rate_slider_text.place(x=refresh_rate_lf.winfo_width() / 2.9, y=30)
 
 def print_selection(value):
     refresh_rate_slider_text.config(text=str(value)+'Hz')
+    daytripper_config.refresh_rate_Hz = value
 
 refresh_rate_slider = Scale(refresh_rate_lf, from_=1, to=30, length=refresh_rate_lf.winfo_width() * 0.85, showvalue=0, sliderlength=20, orient=HORIZONTAL, command=print_selection)
-refresh_rate_slider.set(daytripper_config.refresh_rate_Hz)
-refresh_rate_slider.pack()
-refresh_rate_slider.place(x=PADDING, y=PADDING)
-root.update()
-
 nr_sensitivity_lf = LabelFrame(settings_lf, text="Sensitivity", width=int((MAIN_WINDOW_WIDTH - PADDING*2) / 4.4), height=SETTING_LF_HEIGHT/2)
-nr_sensitivity_lf.pack()
-nr_sensitivity_lf.place(x=refresh_rate_lf.winfo_x() + refresh_rate_lf.winfo_width() + PADDING, y=0)
-nr_sensitivity_lf.pack_propagate(False) 
-root.update()
-
 tof_timing_budget_lf = LabelFrame(settings_lf, text="Timing Budget", width=int((MAIN_WINDOW_WIDTH - PADDING*2) / 4.4), height=SETTING_LF_HEIGHT/2)
-tof_timing_budget_lf.pack()
-tof_timing_budget_lf.place(x=nr_sensitivity_lf.winfo_x() + nr_sensitivity_lf.winfo_width() + PADDING, y=0)
-tof_timing_budget_lf.pack_propagate(False) 
-root.update()
-
 tof_range_lf = LabelFrame(settings_lf, text="Max Range", width=int((MAIN_WINDOW_WIDTH - PADDING*2) / 4.4), height=SETTING_LF_HEIGHT/2)
-tof_range_lf.pack()
-tof_range_lf.place(x=tof_timing_budget_lf.winfo_x() + tof_timing_budget_lf.winfo_width() + PADDING, y=0)
-tof_range_lf.pack_propagate(False) 
-root.update()
 
 # updates_lf = LabelFrame(root, text="Updates", width=int(MAIN_WINDOW_WIDTH*0.2) - PADDING, height=HEIGHT_CONNECT_LF)
-# updates_lf.pack()
 # updates_lf.place(x=int(MAIN_WINDOW_WIDTH*0.8), y=50)
-# updates_lf.pack_propagate(False) 
 # root.update()
 
-# MORE BUTTONS: APPLY, RESET, check update
-# --------------------------
+def enable_widgets():
+    serial_apply_button.config(state=NORMAL)
+    serial_reset_all_button.config(state=NORMAL)
+
+def show_settings():
+    hwif_lf.place(x=PADDING, y=50)
+    hwif_label.place(x=0, y=0)
+    root.update()
+    settings_lf.place(x=PADDING, y=100)
+    root.update()
+    refresh_rate_lf.place(x=PADDING, y=0)
+    root.update()
+    nr_sensitivity_lf.place(x=refresh_rate_lf.winfo_x() + refresh_rate_lf.winfo_width() + PADDING, y=0)
+    root.update()
+    tof_timing_budget_lf.place(x=nr_sensitivity_lf.winfo_x() + nr_sensitivity_lf.winfo_width() + PADDING, y=0)
+    root.update()
+    tof_range_lf.place(x=tof_timing_budget_lf.winfo_x() + tof_timing_budget_lf.winfo_width() + PADDING, y=0)
+    root.update()
+    refresh_rate_slider.config(from_=1, to=30, length=refresh_rate_lf.winfo_width() * 0.85, showvalue=0, sliderlength=20, orient=HORIZONTAL, command=print_selection)
+    refresh_rate_slider.set(daytripper_config.refresh_rate_Hz)
+    refresh_rate_slider.place(x=PADDING, y=PADDING)
+    root.update()
+    refresh_rate_slider_text.place(x=refresh_rate_lf.winfo_width() / 2.9, y=30)
+    root.update()
+
+serial_dropdown_refresh()
+# serial_connect()
+
 def repeat_func():
     root.after(500, repeat_func)
 
