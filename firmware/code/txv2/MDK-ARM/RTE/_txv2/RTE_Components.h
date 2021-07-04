cd6dad9e2334eeadab43ce5af99b84dd8901a26e
dekunukem <dekunukem@gmail.com>
2021-07-04 21:08:22 +0100

    started txv2 firmware

---
+++ firmware/code/txv2/MDK-ARM/RTE/_txv2/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'txv2' 
+ * Target:  'txv2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ firmware/code/txv2/MDK-ARM/txv2/startup_stm32l052xx.d
@@ -0,0 +1,1 @@
+txv2\startup_stm32l052xx.o: startup_stm32l052xx.s
